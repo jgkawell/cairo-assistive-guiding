519eb76cd999792f022599779f1435c7474107f1
Jack Kawell <jgkawell@live.com>
2019-01-16 11:30:56 -0700

    Changed to arrow key navigation for simpleRC

--- RobotGridWorldSimulator2/Python/simpleRC.py
+++ RobotGridWorldSimulator2/Python/simpleRC.py
@@ -5,27 +5,46 @@
 # Python 2 and 3 compatibility. Use Python 3 syntax
 from __future__ import absolute_import, division, print_function
 try:
-      input=raw_input # Python 3 style input()
+    input = raw_input  # Python 3 style input()
 except:
-      pass
+    pass
+
+
+# Set up curses for key input (from https://www.codehaven.co.uk/using-arrow-keys-with-inputs-python/)
+import curses
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
 
 # You can use the demo map file "Maze.map", or create your own by
 # mouse clicking on the map area to raise/lower walls.
-
-# Initialise a new GRobot
 from grobot import *
 
-robot=NewRobot("robot", colour="yellow")
+# Initialise a new GRobot
+
+robot = NewRobot("robot", colour="yellow")
 # You can call your robot any name you want!
 # Normal Python variable rules apply (no spaces, chars aA-zZ, etc.)
 # Colours can be: “red”, “green”, “blue”, “orange” etc.
 
-msg=""
-while msg!="q" and msg!="Q":
-    msg = input("Type: W=Forward, D=Turn Right, A=Turn Left. The press RETURN: ")
-    if msg == "w" or msg == "W": robot.forward()
-    if msg == "a" or msg == "A": robot.left()
-    if msg == "d" or msg == "D": robot.right()
-    if msg == "i" or msg == "I": robot.init()
-    robot.look()
-exit()
+try:
+    while True:
+        char = screen.getch()
+        if char == 'q' or char == 'Q':
+            break
+        elif char == curses.KEY_RIGHT:
+            result = robot.right()
+        elif char == curses.KEY_LEFT:
+            result = robot.left()
+        elif char == curses.KEY_UP:
+            result = robot.forward()
+
+        result = robot.look()
+finally:
+    # shut down cleanly
+    curses.nocbreak()
+    screen.keypad(0)
+    curses.echo()
+    curses.endwin()
+    exit()
