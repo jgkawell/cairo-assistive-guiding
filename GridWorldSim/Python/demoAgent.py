9b146b643bc1329f2bae7489acc10aadeaaa2fee
Sumeet Batra <sumeetbatra@cu-engr2-1-10-10.201.38.29.int.colorado.edu>
2019-01-28 16:17:29 -0700

    updated path-planning.py to print a trace of (x,y) coordinates instead of vertex keys, added setup graph function in Graph() class to automatically create the graph instead of doing it manually, demoAgent.py runs A-Star and the robot can reach the exit autonomously!

--- GridWorldSim/Python/demoAgent.py
+++ GridWorldSim/Python/demoAgent.py
@@ -7,20 +7,22 @@ except:
 
 
 # Set up curses for key input (from https://www.codehaven.co.uk/using-arrow-keys-with-inputs-python/)
-import curses
-screen = curses.initscr()
-curses.noecho()
-curses.cbreak()
-screen.keypad(True)
+# import curses
+# screen = curses.initscr()
+# curses.noecho()
+# curses.cbreak()
+# screen.keypad(True)
 
 # Setup imports
 from grobot import GRobot
+from path_planning import *
 import pickle
+
 
 # Initialise globals
 robot = GRobot("simpleRC", colour="yellow")
 defaultWorld = "./../Maps/MazeExtra.map" # this must be the same as that used in RobotGridWorld.pyw (and any other agents operating together)
-mapsize = 30
+mapsize = 31
 
 # Import map for data
 world = pickle.load(open(defaultWorld, 'rb'))
@@ -32,35 +34,62 @@ for i in range(0, mapsize):
         if world[i][j] == "Hazard":
             world[i][j] = None
 
-try:
-    while True:
-        char = screen.getch()
-        if char == 'q' or char == 'Q':
-            break
-        elif char == curses.KEY_RIGHT:
-            result = robot.right()
-        elif char == curses.KEY_LEFT:
-            result = robot.left()
-        elif char == curses.KEY_UP:
-            result = robot.forward()
+#path plan with a*
+G = Graph(mapsize, world)
+start = G.get_vertex(31*2 + 2)
+goal = G.get_vertex(31*16 + 2) #goal pos for MazeExtra.map
+t = a_star(G, start, goal)
+print(reversed(t))
 
-        result = robot.look()
+heading = 90 #0=forward, 90 = right, 180 = down, 270 = left
+for coord in reversed(t):
+    (i, j) = coord
+    (x, y) = (i-1, j-1)
+    direction = (x - robot.posx, y - robot.posy)
+    print((x,y), (robot.posx, robot.posy))
+    if direction == (1, 0): #right
+        if heading == 0:
+            robot.forward()
+        else:
+            for i in range(int(heading / 90)): robot.right()
+            robot.forward()
+            heading = 0
+        robot.posx += 1
 
-        # pull out values from world based on result coords
-        cells = []
-        for cell in result:
-            x = cell[1]
-            y = cell[2]
-            cells.append((world[x][y], x, y))
-            
-        # print out values from look and from world (note they are the same)
-        screen.erase()
-        screen.addstr(0, 0, str(result))
-        screen.addstr(1, 0, str(cells))
-finally:
-    # shut down cleanly
-    curses.nocbreak()
-    screen.keypad(0)
-    curses.echo()
-    curses.endwin()
-    exit()
+    elif direction == (0, 1): #up
+        if heading == 90:
+            robot.forward()
+        elif heading > 90:
+            for i in range(int((heading - 90)/90)): robot.right()
+            robot.forward()
+            heading = 90
+        else: #facing right
+            robot.left()
+            robot.forward()
+            heading = 90
+        robot.posy += 1
+
+    elif direction == (-1, 0): #left
+        if heading == 180:
+            robot.forward()
+        elif heading < 180:
+            for i in range(int((180-heading)/90)): robot.left()
+            robot.forward()
+            heading = 180
+        else: # facing down = 270
+            robot.right()
+            robot.forward()
+            heading = 180
+        robot.posx -= 1
+
+    elif direction == (0, -1): #down
+        if heading == 270:
+            robot.forward()
+        else:
+            for i in range(int((270-heading)/90)): robot.left()
+            robot.forward()
+            heading = 270
+        robot.posy -= 1
+
+    else:
+        pass
--- GridWorldSim/Python/path_planning.py
+++ GridWorldSim/Python/path_planning.py
@@ -31,8 +31,8 @@ class Vertex():
         return self.adj_list.keys()
 
     def get_xy(self, mapsize):
-        y = self.key % mapsize
-        x = int((self.key - y) / mapsize)
+        x = self.key % mapsize
+        y = int((self.key - x) / mapsize)
         return (x, y)
 
     def __lt__(self, other):
@@ -47,10 +47,30 @@ class Vertex():
         return hash(self.key)
 
 class Graph():
-    def __init__(self, mapsize, directed=False):
+    def __init__(self, mapsize, world, directed=False):
         self.vertices = {}
         self.num_walls = 0
         self.mapsize = mapsize
+        self.setup_graph(world, self.mapsize)
+
+    def setup_graph(self, world, mapsize):
+
+        for i in range(0, mapsize):
+            for j in range(0, mapsize):
+                key = i + mapsize * j
+                celltype = world[i][j]
+                v = Vertex(key, celltype)
+                self.add_vertex(v)
+
+        for i in range(mapsize**2):
+            v_cur = self.get_vertex(i)
+            if v_cur.cell_type != "Wall": # 'i' rows by 'j' columns, key = mapsize * i + j
+                if i % mapsize != mapsize-1 and self.get_vertex(i+1).cell_type != "Wall": self.add_edge(i, i+1) #try to add cell to the right
+                if i % mapsize != 0 and self.get_vertex(i-1).cell_type != "Wall": self.add_edge(i, i-1) #cell to the left
+                if i + mapsize < mapsize**2 and self.get_vertex(i+mapsize).cell_type != "Wall": self.add_edge(i, i+mapsize) #cell above
+                if i - mapsize > 0 and self.get_vertex(i-mapsize).cell_type != "Wall": self.add_edge(i, i-mapsize) #cell below
+
+
 
     def add_vertex(self, vertex):
         self.vertices[vertex.key] = vertex
@@ -75,11 +95,12 @@ def heuristic(goal_pos, vertex_pos): #manhattan distance - admissible
 
 def trace(vertex, graph):
     trace = []
-    trace.append(vertex.key)
+    trace.append(vertex.get_xy(graph.mapsize))
     curr = vertex
     while curr.parent != -1:
-        trace.append(curr.parent)
-        curr = graph.get_vertex(curr.parent)
+        v_parent = graph.get_vertex(curr.parent)
+        trace.append(v_parent.get_xy(graph.mapsize))
+        curr = v_parent
     return trace
 
 def a_star(graph, start, goal):
@@ -107,8 +128,7 @@ def a_star(graph, start, goal):
             if neighbor not in frontier_tracker and neighbor not in cost_so_far:
                 neighbor.dist = cost
                 frontier_tracker[neighbor] = cost
-                priority = cost + heuristic(goal.get_xy(mapsize), neighbor.get_xy(mapsize))
+                priority = cost + heuristic(goal.get_xy(graph.mapsize), neighbor.get_xy(graph.mapsize))
                 frontier.put(neighbor, priority)
                 neighbor.parent = current.key
     return trace(goal, graph)
-            
