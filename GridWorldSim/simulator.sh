f44096696c16d16ae11e19c60535f351e65128e1
Jack Kawell <jgkawell@outlook.com>
2019-03-01 12:12:55 -0700

    Changed sim to run only human

--- GridWorldSim/simulator.sh
+++ GridWorldSim/simulator.sh
@@ -5,10 +5,10 @@ do
     ./GridWorldSim.pyw &
     sleep 1
     # sys.argv: 0=name, 1=planner, 2=optimal
-    python3 humanSim.py True False & 
-    sleep 1
+    python3 humanSim.py False False & 
+    # sleep 1
     # sys.argv: 0=name, 1=abstract, 2=probabilistic_model
-    python3 robotSim.py True True &
+    # python3 robotSim.py True True &
 
     wait
 done
